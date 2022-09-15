9bf7412abe9ea3d368c1695f1782b4d9a8949cd0
btxmkbtx <btxmkbtx@gmail.com>
2022-09-16 03:48:21 +0900

    编程路由实例改善

--- 20_2_编程式路由_hooks方式_V5版本/pages/Home/index.jsx
+++ 20_2_编程式路由_hooks方式_V5版本/pages/Home/index.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Home() {
-  return <div>home</div>;
+  const history = useHistory();
+  const goAbout = () => {
+    history.push("/about");
+  };
+
+  return (
+    <div>
+      <div>我是Home</div>
+      <button onClick={goAbout}>找About请点击我</button>
+    </div>
+  );
 }
